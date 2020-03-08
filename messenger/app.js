8186a51a8909953e2def47b8201ab0ec961ff08c
neerajp99 <neerajp1999@gmail.com>
2020-03-09 03:58:37 +0530

    Initialise app

---
+++ messenger/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+// Initialise express app
+const app = express();
+
+// Testing route
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.post("/webhook", (req, res) => {
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === "page") {
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+      // Get the webhook event. entry.messaging is an array, but
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+});
+
+
+
+// Initalise port
+const port = 7010 | process.env.PORT;
+
+app.listen(port, () => {
+  console.log("App is listening at port ", port);
+});
